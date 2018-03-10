f0c2f8896cf6134d7cf007c55da9ca39c4d276b8
Torsten Kemps-Benedix <torsten.kemps-benedix@sks-group.eu>
2018-03-10 14:35:44 +0100

    colley calculated together with massey

--- massey.R
+++ massey.R
@@ -21,35 +21,68 @@ emat <- function(n,m,i,j,x=1) {
 	m[i,j] <- x
 	m
 }
+spiel[, .(points=sum(Punkte_Heim)), by=Heim][order(Heim)]$points
+massey <- function(spiel, measure=c("g","p")) {
+	if (measure=="g") {
+		spiel[, `:=`(mH = Tore_Heim, mG = Tore_Gast)]
+	} else {
+		spiel[, `:=`(mH = Punkte_Heim, mG = Punkte_Gast)]	}
 
-h <- spiel[, .N, by = Heim]
-g <- spiel[, .N, by = Gast]
+	h <- spiel[, .N, by = Heim]
+	g <- spiel[, .N, by = Gast]
 
-H <- Reduce('+', mapply(function(i,m) emat(n,n,i,i)*m, h$Heim, h$N, SIMPLIFY=F))
-G <- Reduce('+', mapply(function(i,m) emat(n,n,i,i)*m, g$Gast, g$N, SIMPLIFY=F))
-T <- H+G
-P <- Reduce('+', mapply(function(i,j) emat(n,n,i,j), spiel$Gast, spiel$Heim, SIMPLIFY=F))
-M <- T-P
-M1 <- M
-M1[n,] <- 1
+	H <- Reduce('+', mapply(function(i,m) emat(n,n,i,i)*m, h$Heim, h$N, SIMPLIFY=F))
+	G <- Reduce('+', mapply(function(i,m) emat(n,n,i,i)*m, g$Gast, g$N, SIMPLIFY=F))
+	T <- H+G
+	P <- Reduce('+', mapply(function(i,j) emat(n,n,i,j), spiel$Gast, spiel$Heim, SIMPLIFY=F))
+	M <- T-P
+	M1 <- M
+	M1[n,] <- 1
 
-pH <- spiel[, .(D=sum(Tore_Heim-Tore_Gast)), by=Heim][order(Heim)][,D]
-pG <- spiel[, .(D=-sum(Tore_Heim-Tore_Gast)), by=Gast][order(Gast)][,D]
-p <- pH+pG
-p1 <- p
-p1[n] <- 0
+	pH <- spiel[, .(D=sum(mH-mG)), by=Heim][order(Heim)][,D]
+	pG <- spiel[, .(D=-sum(mH-mG)), by=Gast][order(Gast)][,D]
+	p <- pH+pG
+	p1 <- p
+	p1[n] <- 0
 
-r <- solve(M1,p1)
+	r <- solve(M1,p1)
 
-f <- spiel[, .(f=sum(Tore_Heim)), by=Heim][order(Heim), .(f)] + spiel[, .(f=sum(Tore_Gast)), by=Gast][order(Gast), .(f)]
-a <- spiel[, .(a=sum(Tore_Gast)), by=Heim][order(Heim), .(a)] + spiel[, .(a=sum(Tore_Heim)), by=Gast][order(Gast), .(a)]
+	f <- spiel[, .(f=sum(mH)), by=Heim][order(Heim), .(f)] + spiel[, .(f=sum(mG)), by=Gast][order(Gast), .(f)]
+	a <- spiel[, .(a=sum(mG)), by=Heim][order(Heim), .(a)] + spiel[, .(a=sum(mH)), by=Gast][order(Gast), .(a)]
 
-d <- solve(T+P, as.vector(T %*% r - f$f))
-o <- r-d
+	d <- solve(T+P, as.vector(T %*% r - f$f))
+	o <- r-d
 
-massey <- data.table(Verein=verein$verein, Ranking=r, Off=o, Def=d, F=f$f, A=a$a, Heim=pH, Gast=pG, `Heim+Gast`=pH+pG)[order(Ranking, decreasing=TRUE)]
-massey
+	w <- spiel[Punkte_Heim==3, .N, by=Heim][order(Heim)]$N + spiel[Punkte_Gast==3, .N, by=Gast][order(Gast)]$N
+	l <- spiel[, .(N = sum(Punkte_Heim==0)), by=Heim][order(Heim)]$N + spiel[Punkte_Gast==0, .N, by=Gast][order(Gast)]$N
+	b <- (w-l)*0.5+1
+	C <- 2*diag(n)+M
+	colleyRating <- solve(C,b)
 
-ggplot(massey, aes(Off, Def)) +
+	points <- spiel[, .(points=sum(Punkte_Heim)), by=Heim][order(Heim)]$points +spiel[, .(points=sum(Punkte_Gast)), by=Gast][order(Gast)]$points
+
+	massey <- data.table(Verein=verein$verein, masseyRating=r, colleyRating=colleyRating, Off=o, Def=d, 
+						  F=f$f, A=a$a, Heim=pH, Gast=pG, 
+						  `Heim+Gast`=pH+pG, wins=w, losses=l, points=points)[order(masseyRating, decreasing=TRUE)]
+	massey
+}
+
+
+print(mr <- massey(spiel, measure="g"))
+summary(lm_mr <- lm(masseyRating~points, data=mr))
+summary(lm_cr <- lm(colleyRating~points, data=mr))
+
+ggplot(mr, aes(Off, Def)) +
 	geom_point() +
     geom_text(aes(Off, Def+0.015, label=Verein))
+
+ggplot(mr, aes(y=masseyRating, x=points)) +
+	geom_abline(aes(slope=lm_mr$coefficients[2], intercept= lm_mr$coefficients[1]), color="magenta") +
+	geom_point(color="blue") +
+    geom_text(aes(y=masseyRating+0.03, x=points, label=Verein), color="darkblue")
+
+#ggplot(mr, aes(y=colleyRating, x=points)) +
+#	geom_abline(aes(slope=lm_cr$coefficients[2], intercept= lm_cr$coefficients[1]), color="magenta") +
+#	geom_point(color="blue") +
+#	geom_text(aes(y=colleyRating+0.0075, x=points, label=Verein), color="darkblue")
+
